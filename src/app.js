2da6e27a737eb0233534110d516163805a04dc6a
telkomdev-vedo <vedoalfarizi@gmail.com>
2022-01-02 12:50:17 +0700

    add unhandledRejection error handler

--- src/app.js
+++ src/app.js
@@ -2,6 +2,11 @@ require('dotenv').config();
 const createServer = require('./Infrastructures/http/createServer');
 const container = require('./Infrastructures/container');
 
+process.on('unhandledRejection', (err) => {
+  console.log(err);
+  process.exit(1);
+});
+
 (async () => {
   const server = await createServer(container);
   await server.start();
